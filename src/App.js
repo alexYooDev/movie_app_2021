54e0c87ece0830d93a386efe31520fa94d750205
fm247 <hiyoo95@gmail.com>
2021-05-17 20:47:36 +0900

    branch 2_1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({ favorite }) {
+  return <h2>I like {favorite}</h2>; // favorite 객체 안의 정보: prop를 component를 통해 인자로 받아 보낸다
+}
 
 function App() {
+  //component HTML을 반환하는 함수
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      <Food favorite="Fajita" />
+      <Food favorite="Ramen" />
+      <Food favorite="Chocolate" />
+      <Food favorite="Ice Cream" />
+      <Food favorite="Steak" />
+      <Food favorite="Burger" />
     </div>
   );
 }
-
+//prop : 컴포넌트에게 정보를 주기 위한 것 (attribute 형태)
+//컴포넌트는 대문자로 시작해야 함, jsx = html과 javascript의 결합
+//위의 favorite은 prop(Food)안의 객체이고 이를 통해 정보를 자식 함수에 보냄
 export default App;
