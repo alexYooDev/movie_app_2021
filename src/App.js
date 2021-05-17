6f07deee17513274d4f6d91559f9bd15ae9ba518
fm247 <hiyoo95@gmail.com>
2021-05-17 22:38:57 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import React from 'react';
 
-function Food({ favorite }) {
-  return <h2>I like {favorite}</h2>; // favorite 객체 안의 정보: prop를 component를 통해 인자로 받아 보낸다
+function Food({ name, image }) {
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <img src={image}></img>
+    </div>
+  ); // favorite 객체 안의 정보: prop를 component를 통해 인자로 받아 보낸다
 }
 
+const foodILike = [
+  {
+    name: 'fajita',
+    image:
+      'https://images.themodernproper.com/billowy-turkey/production/posts/2019/Chicken-Fajitas-7.jpg?w=1200&auto=compress%2Cformat&fit=crop&fp-x=0.5&fp-y=0.5&dm=1599767214&s=926a97efc213736e02cdbc2bc0cbd2f9',
+  },
+  {
+    name: 'ice cream',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoWqvg87qaMqHwEdeazfn_xcSgz-Eo1z5AAQ&usqp=CAU',
+  },
+  {
+    name: 'sushi',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_e_CRh21r_PAMLCWJCNJWXwGmVLrngpvO-g&usqp=CAU',
+  },
+];
+//map: 배열을 받아서 배열을 반환하는 함수
 function App() {
   //component HTML을 반환하는 함수
   return (
     <div>
-      <h1>Hello</h1>
-      <Food favorite="Fajita" />
-      <Food favorite="Ramen" />
-      <Food favorite="Chocolate" />
-      <Food favorite="Ice Cream" />
-      <Food favorite="Steak" />
-      <Food favorite="Burger" />
+      {foodILike.map((food) => (
+        <Food name={food.name} image={food.image} />
+      ))}
     </div>
   );
 }
